0cae0e453faa8001b0924f7440bbb0d7b407859b
Rui Nascimento <sruy19@gmail.com>
2024-04-01 17:51:32 -0300

    implementando L do solid

--- estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/CalculadoraDeEstatisticas.java
+++ estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/CalculadoraDeEstatisticas.java
@@ -7,32 +7,35 @@ import cotuba.plugin.Plugin;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.text.Normalizer;
+
 public class CalculadoraDeEstatisticas implements Plugin {
 
-  @Override
-  public String aposRenderizacao(String html) {
-    return html;
-  }
-
-  @Override
-  public void aposGeracao(Ebook ebook) {
-
-    for (Capitulo capitulo : ebook.getCapitulos()) {
-
-      String html = capitulo.getConteudoHTML();
-
-      Document doc = Jsoup.parse(html);
-
-      String textoDoCapitulo = doc.body().text();
-
-      String[] palavras = textoDoCapitulo.split("\\s+");
-
-      for (String palavra : palavras) {
-        System.out.println(palavra);
-      }
-
+    @Override
+    public String aposRenderizacao(String html) {
+        return html;
     }
 
-  }
+    @Override
+    public void aposGeracao(Ebook ebook) {
+
+        for (Capitulo capitulo : ebook.getCapitulos()) {
+
+            String textoSemPontuacao = capitulo.getConteudoHTML().replaceAll("\\p{Punct}", "\\s");
+            String decomposta = Normalizer.normalize(textoSemPontuacao, Normalizer.Form.NFD);
+            String textoSemAcentos = decomposta.replaceAll("[^\\p{ASCII}]", "");
+            Document doc = Jsoup.parse(textoSemAcentos);
+
+            String textoDoCapitulo = doc.body().text();
+
+            String[] palavras = textoDoCapitulo.split("\\s+");
+
+            for (String palavra : palavras) {
+                System.out.println(palavra);
+            }
+
+        }
+
+    }
 
 }
